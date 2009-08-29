c89efd0c3bdeedb90330c6d5f10d33d6a5c2e3e2
David Caldwell <david@porkrind.org>
2009-08-29 14:56:10 -0700

    [round.h] Add round_down().

--- round.h
+++ round.h
@@ -3,6 +3,7 @@
 #define __ROUND_H__
 
 #define divide_round_up(val,divisor) (((val)+(divisor)-1)/(divisor))
+#define round_down(val,divisor) ((val)/(divisor)*(divisor))
 
 #endif /* __ROUND_H__ */
 
